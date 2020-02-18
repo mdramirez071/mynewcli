91c081b44185c1c33fcc0dd7b20bc5cbcf44f38d
mdramirez071 <mdramirez071@gmail.com>
2020-02-17 17:45:30 -0800

    adds unit test to mocha

--- test/index.test.js
+++ test/index.test.js
@@ -11,6 +11,13 @@ describe('mynewcli', () => {
 
   test
   .stdout()
+  .do(() => cmd.run([]))
+  .it('runs Welcome', ctx => {
+    expect(ctx.stdout).to.contain('Welcome to my first single-CLI')
+  })
+
+  test
+  .stdout()
   .do(() => cmd.run(['--name', 'jeff']))
   .it('runs hello --name jeff', ctx => {
     expect(ctx.stdout).to.contain('hello jeff')
